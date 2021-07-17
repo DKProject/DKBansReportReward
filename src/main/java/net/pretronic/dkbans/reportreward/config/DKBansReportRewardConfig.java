8014fc3dc7cec6b813ae528395199f030b055284
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-07-17 19:44:12 +0200

    Added Jenkinsfile

--- src/main/java/net/pretronic/dkbans/reportreward/config/DKBansReportRewardConfig.java
+++ src/main/java/net/pretronic/dkbans/reportreward/config/DKBansReportRewardConfig.java
@@ -10,7 +10,7 @@ public class DKBansReportRewardConfig {
     public static double REWARD_MAX = 100.0;
 
     public static String TRANSACTION_CURRENCY = "Coins";
-    public static String TRANSACTION_CAUSE = "REPORT_REWARD";
+    public static String TRANSACTION_CAUSE = "Reward";
     public static String TRANSACTION_REASON = "Reward for successful report ({player})";
 
     public transient static Currency CURRENCY;
